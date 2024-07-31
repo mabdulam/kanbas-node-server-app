6ce5537e15d1cd0a9b96c1c1fe6c6c820ec65dd1
mabdulam <mohammed.abdul-ameer@outlook.com>
2024-07-31 12:10:23 -0400

    First commit for a6

--- App.js
+++ App.js
@@ -5,16 +5,41 @@ import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import cors from "cors";
 import AssignmentRoutes from './Kanbas/Assignments/routes.js';
+import mongoose from "mongoose";
+import "dotenv/config";
+import UserRoutes from "./User/routes.js";
+import session from "express-session";
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
-app.use(cors());
+app.use(cors({   credentials: true,
+  origin: process.env.NETLIFY_URL || "http://localhost:3000",}));
 app.use(express.json());
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET || "kanbas",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
+  };
+}
+app.use(
+  session(sessionOptions)
+);
 
 Hello(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
 Lab5(app);
+UserRoutes(app);
 
 app.listen(process.env.PORT || 4000, () => {
   console.log(`Server is running on port ${process.env.PORT || 4000}`);
