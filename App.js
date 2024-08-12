61999a4ed6b0961d642e324681ff68a819ba25e2
mabdulam <mohammed.abdul-ameer@outlook.com>
2024-08-12 00:07:18 -0400

    app.js updated

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ const sessionOptions = {
   resave: false,
   saveUninitialized: false,
 };
-if (process.env.NODE_ENV !== "development") {
+if (process.env.NODE_ENV !== "production") {
   sessionOptions.proxy = true;
   sessionOptions.cookie = {
     sameSite: "none",
